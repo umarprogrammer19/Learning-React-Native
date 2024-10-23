953fb17f0753d60ecb6c56ad53e15e47f17f1169
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-23 20:15:07 +0500

    Added TouchAbleOpacity

--- 02-Todo-App/app/index.tsx
+++ 02-Todo-App/app/index.tsx
@@ -1,16 +1,18 @@
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
     <View style={TodoContainer}>
       <Text style={TodoHeading}>Todo App.</Text>
       <TextInput placeholder="Enter Todo" style={TodoInput}></TextInput>
-      <Button title="Add Item"></Button>
+      <TouchableOpacity style={TodoButton}>
+        <Text style={TodoButtonText}>Add Item</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const { TodoContainer, TodoHeading, TodoInput } = StyleSheet.create({
+const { TodoContainer, TodoHeading, TodoInput, TodoButton, TodoButtonText } = StyleSheet.create({
   TodoContainer: {
     flex: 1,
     backgroundColor: "black",
@@ -28,5 +30,15 @@ const { TodoContainer, TodoHeading, TodoInput } = StyleSheet.create({
     borderColor: "yellow",
     padding: 10,
     color: "white",
+  },
+  TodoButton: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    height: 40,
+    margin: 12,
+    padding: 7.75,
+  },
+  TodoButtonText: {
+    fontSize: 18,
   }
 })
