6e3457daeb64c0b35082e83f207e44ede640ebe6
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-23 20:03:05 +0500

    Added Textr Input

--- 02-Todo-App/app/index.tsx
+++ 02-Todo-App/app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={TodoContainer}>
+      <Text style={TodoHeading}>Todo App.</Text>
+      <TextInput placeholder="Enter Todo" style={TodoInput}></TextInput>
     </View>
   );
 }
+
+const { TodoContainer, TodoHeading, TodoInput } = StyleSheet.create({
+  TodoContainer: {
+    flex: 1,
+    backgroundColor: "black",
+    paddingTop: 20,
+  },
+  TodoHeading: {
+    textAlign: "center",
+    fontSize: 25,
+    color: "yellow",
+  },
+  TodoInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 3,
+    borderColor: "yellow",
+    padding: 10,
+  }
+})
