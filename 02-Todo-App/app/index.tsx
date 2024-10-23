8d096776300a5f56412b9f0b11154367bf2216eb
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-23 20:41:18 +0500

    Add Successfull

--- 02-Todo-App/app/index.tsx
+++ 02-Todo-App/app/index.tsx
@@ -1,26 +1,51 @@
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, FlatList, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+interface ItemProps {
+  title: string;
+}
+
+const Item = ({ title }: ItemProps) => (
+  <View style={Todoitem}>
+    <Text style={TodoTitle}>{title}</Text>
+  </View>
+);
 
 export default function Index() {
-  
+
   // State For Getting Text
   const [text, setText] = useState<string>("");
   // State For Getting Todo List Items 
-  const [todo, setTodo] = useState<string[]>([]);
+  const [todo, setTodo] = useState<string[]>(["Hello"]);
 
+  const addTodo = (): void => {
+    todo.push(text);
+    setTodo([...todo]);
+  }
 
   return (
     <View style={TodoContainer}>
       <Text style={TodoHeading}>Todo App.</Text>
       <TextInput placeholder="Enter Todo" style={TodoInput} value={text} onChangeText={setText}></TextInput>
-      <TouchableOpacity style={TodoButton}>
+      <TouchableOpacity style={TodoButton} onPress={addTodo}>
         <Text style={TodoButtonText}>Add Item</Text>
       </TouchableOpacity>
+      {todo.length > 0 && <View>
+        <SafeAreaView>
+          <FlatList
+            data={todo}
+            renderItem={({ item }) => <Item title={item} />}
+            keyExtractor={index => index}
+          />
+        </SafeAreaView>
+      </View>}
+
     </View>
   );
 }
 
-const { TodoContainer, TodoHeading, TodoInput, TodoButton, TodoButtonText } = StyleSheet.create({
+const { TodoContainer, TodoHeading, TodoInput, TodoButton, TodoButtonText, Todoitem, TodoTitle } = StyleSheet.create({
   TodoContainer: {
     flex: 1,
     backgroundColor: "black",
@@ -48,5 +73,16 @@ const { TodoContainer, TodoHeading, TodoInput, TodoButton, TodoButtonText } = St
   },
   TodoButtonText: {
     fontSize: 18,
-  }
+  },
+  Todoitem: {
+    backgroundColor: '#f9c2ff',
+    height: 45,
+    marginHorizontal: 12,
+    marginBottom: 15,
+  },
+  TodoTitle: {
+    textAlign: "center",
+    marginTop: 8,
+    fontSize: 20,
+  },
 })
